a21a5add32f7e1fe0a30bbc7e3b8cb0e3f0b52ea
SurovtsevaVictoriia <78654989+SurovtsevaVictoriia@users.noreply.github.com>
2021-04-08 18:16:51 +0300

    in process (doesnt work yet)

--- message.cpp
+++ message.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <thread>
 #include <mutex>
+#include <chrono>
 
 
 #include <boost/interprocess/allocators/allocator.hpp>
@@ -9,35 +10,62 @@
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/sync/named_mutex.hpp>
 
+using chat = std::vector <struct Message>;
+
 int Id = -1;
 //make global variable for isned(= true)
 
 bool IsEnded = false;
 
 struct Message {
+
+    Message() {};
+    Message(std::string text_,int sender_id) {
+        sender = sender_id;
+        text = text_;
+    }
+   
+
     std::string text;
     unsigned int sender;
 };
 
-void thread_func_read() {
+std::ostream& operator<< (std::ostream& out, const Message& message) {
+
+    return out << "sender: " << message.sender << "\n " << message.text << "\n";
+};
+
+void thread_func_read( std::string shared_memory_name, chat& Chat, boost::interprocess::named_mutex& m_mutex) {
 
     if (IsEnded == true) return;
     //when write-mutex unlocks, lock mutex, read(last object in container), cout, then unlock, then sleep.
      //kill process (?)
-    
+
+
+    m_mutex.lock();
+    std::cout << Chat.back();
+    m_mutex.unlock();
+
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(100ms);
+
+
     //after sleep (if is ended  = true)  
 }
 
-void thread_func_write(std::string shared_memory_name) {
+void thread_func_write(int id, std::string shared_memory_name, chat& Chat, boost::interprocess::named_mutex & m_mutex) {
 
-
+    
 
     std::string text;
     while(std::getline(std::cin, text)){
 
         //lock mutex
+        m_mutex.lock();
         //put text into container located in shared memory. 
+        Chat.push_back(Message(text, id));
         //(pushback in container)
+        m_mutex.unlock();
         //unlock mutex    
     }
 
@@ -57,31 +85,21 @@ int main(int argc, char** argv) {
 
     Id = atoi(argv[0]);
     std::cout << " process ID: " << Id << "\n";
+   
 
-    
+    const std::string mutex_name = "m_mutex";
 
-//    boost::interprocess::managed_shared_memory managed_shm(boost::interprocess::create_only, "MySharedMemory", 10000 * sizeof(char));
-
-
-    const std::string mutex_name = "mutex";
-
-    boost::interprocess::named_mutex mutex(
+    boost::interprocess::named_mutex m_mutex(
         boost::interprocess::open_or_create, mutex_name.c_str());
 
-    system("pause");
+   // system("pause");
 
     const std::string shared_memory_name = "shared_memory";
-
-   // boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str()); // !
 
     boost::interprocess::managed_shared_memory shared_memory(
         boost::interprocess::open_or_create, shared_memory_name.c_str(), 10000 * sizeof(char));
 
-   //shared_memory.truncate(1024*1024*sizeof(char));
-
     boost::interprocess::mapped_region region(shared_memory, boost::interprocess::read_write);
-
-   // std::memset(region.get_address(), 1, region.get_size()); // !
 
     std::cout << "shared memory ready" << std::endl;
 
