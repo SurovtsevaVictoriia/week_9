0e609780005ca957226b2bbc5d5fc8f9390d4c42
SurovtsevaVictoriia <78654989+SurovtsevaVictoriia@users.noreply.github.com>
2021-04-08 20:21:29 +0300

    a mess

--- message.cpp
+++ message.cpp
@@ -14,10 +14,18 @@ using chat = std::vector <struct Message>;
 
 int Id = -1;
 
+////how many unread messges process has(actually needs to be in shm)
+//int New_Messages = 0;
 
 //if true then process wont be sendeing messages anymore
 bool IsEnded = false;
 
+
+const std::string shared_memory_name = "n_shared_memory";
+const std::string mutex_name = "n_mutex";
+const std::string process_num_name = "n_process_num";
+const std::string new_messages_name = "n_new_msgs";
+const std::string Chat_name = "n_Chat";
 //message struct
 struct Message {
 
@@ -38,30 +46,41 @@ std::ostream& operator<< (std::ostream& out, const Message& message) {
 };
 
 //reads last messages and outputs on screen
-void thread_func_read( std::string shared_memory_name, chat& Chat, boost::interprocess::named_mutex& m_mutex) {
+void thread_func_read() {
 
-    if (IsEnded == true) return;
-  
-    m_mutex.lock();
-    std::cout << Chat.back();
-    m_mutex.unlock();
+    using namespace boost::interprocess;
+    managed_shared_memory segment(open_only, shared_memory_name.c_str());
 
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for(100ms);
-    return;
+    while (true) {
+        if (IsEnded == true) return;
+
+        
+       /* for (int i = 0; i < New_Messages; ++i) {
+            m_mutex.lock();
+            std::cout << Chat[Chat.size() -1 - i];
+            m_mutex.unlock();
+        }*/
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(100ms);
+    }
+        return;
+   
 }
 
 //pushes getlined text in vector in shared memory
-void thread_func_write(int id, std::string shared_memory_name, chat& Chat, boost::interprocess::named_mutex & m_mutex) {
+void thread_func_write() {
 
+    using namespace boost::interprocess;
+    managed_shared_memory segment(open_only, shared_memory_name.c_str());
    
     std::string text;
     while(std::getline(std::cin, text) && (text != "cancel")){
 
-        m_mutex.lock();
+
+        segment.find<boost::interprocess::interprocess_mutex>("n_mutex").first.lock();
         Chat.push_back(Message(text, id));
+        New_Messages++;
         m_mutex.unlock();
-      
     }
 
     IsEnded = true;
@@ -82,25 +101,29 @@ int main(int argc, char** argv) {
     Id = atoi(argv[0]);
     std::cout << " process ID: " << Id << "\n";
    
-    //named mutex created 
-    const std::string mutex_name = "n_mutex";
-    named_mutex m_mutex(
-       open_or_create, mutex_name.c_str());
-
-    //system("pause");
-
+    
+   
+   
     //shread memory created
-    const std::string shared_memory_name = "n_shared_memory";
-    //if (process_num == 1) boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str()); // !
     managed_shared_memory shared_memory(open_or_create, shared_memory_name.c_str(), 10000 * sizeof(char));
 
     mapped_region region(shared_memory,read_write);
 
+    //named mutex created 
+   
+    auto mutex =
+        shared_memory.find_or_construct < boost::interprocess::interprocess_mutex >(
+            mutex_name.c_str())();
+
+
     //process counter
-    int* process_num = shared_memory.find_or_construct<int>("n_process_num")();
+    int* process_num = shared_memory.find_or_construct<int>(process_num_name.c_str())();
     *process_num++;
 
-    chat* ptr = shared_memory.find_or_construct<chat>("n_Chat")();
+    //unread messages counter(?)
+    int* new_messages = shared_memory.find_or_construct<int>(new_messages_name.c_str())();
+    //chat history itself
+    chat* ptr = shared_memory.find_or_construct<chat>(Chat_name.c_str())();
 
     //started separate the thread in process fo reading and writing
     std::thread read_thread(thread_func_read);
