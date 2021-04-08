aa5e5103a3054c664c5a11c3db6c54fb817775d2
SurovtsevaVictoriia <78654989+SurovtsevaVictoriia@users.noreply.github.com>
2021-04-08 19:38:44 +0300

    added threads + cleaned up commets (still not working)

--- message.cpp
+++ message.cpp
@@ -13,10 +13,12 @@
 using chat = std::vector <struct Message>;
 
 int Id = -1;
-//make global variable for isned(= true)
 
+
+//if true then process wont be sendeing messages anymore
 bool IsEnded = false;
 
+//message struct
 struct Message {
 
     Message() {};
@@ -25,51 +27,43 @@ struct Message {
         text = text_;
     }
    
-
     std::string text;
     unsigned int sender;
 };
 
+//output operator
 std::ostream& operator<< (std::ostream& out, const Message& message) {
 
-    return out << "sender: " << message.sender << "\n " << message.text << "\n";
+    return out << "sender process: " << message.sender << "\n " << message.text << "\n";
 };
 
+//reads last messages and outputs on screen
 void thread_func_read( std::string shared_memory_name, chat& Chat, boost::interprocess::named_mutex& m_mutex) {
 
     if (IsEnded == true) return;
-    //when write-mutex unlocks, lock mutex, read(last object in container), cout, then unlock, then sleep.
-     //kill process (?)
-
-
+  
     m_mutex.lock();
     std::cout << Chat.back();
     m_mutex.unlock();
 
     using namespace std::chrono_literals;
     std::this_thread::sleep_for(100ms);
-
-
-    //after sleep (if is ended  = true)  
+    return;
 }
 
+//pushes getlined text in vector in shared memory
 void thread_func_write(int id, std::string shared_memory_name, chat& Chat, boost::interprocess::named_mutex & m_mutex) {
 
-    
-
+   
     std::string text;
-    while(std::getline(std::cin, text)){
+    while(std::getline(std::cin, text) && (text != "cancel")){
 
-        //lock mutex
         m_mutex.lock();
-        //put text into container located in shared memory. 
         Chat.push_back(Message(text, id));
-        //(pushback in container)
         m_mutex.unlock();
-        //unlock mutex    
+      
     }
 
-    // make global variable
     IsEnded = true;
     return;
 }
@@ -77,6 +71,8 @@ void thread_func_write(int id, std::string shared_memory_name, chat& Chat, boost
 
 
 int main(int argc, char** argv) {
+
+    using namespace boost::interprocess;
 
     if (argc != 0) {
         std::cout << "incorrect number of arguments\n";
@@ -86,30 +82,33 @@ int main(int argc, char** argv) {
     Id = atoi(argv[0]);
     std::cout << " process ID: " << Id << "\n";
    
+    //named mutex created 
+    const std::string mutex_name = "n_mutex";
+    named_mutex m_mutex(
+       open_or_create, mutex_name.c_str());
 
-    const std::string mutex_name = "m_mutex";
+    //system("pause");
 
-    boost::interprocess::named_mutex m_mutex(
-        boost::interprocess::open_or_create, mutex_name.c_str());
+    //shread memory created
+    const std::string shared_memory_name = "n_shared_memory";
+    //if (process_num == 1) boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str()); // !
+    managed_shared_memory shared_memory(open_or_create, shared_memory_name.c_str(), 10000 * sizeof(char));
 
-   // system("pause");
+    mapped_region region(shared_memory,read_write);
 
-    const std::string shared_memory_name = "shared_memory";
+    //process counter
+    int* process_num = shared_memory.find_or_construct<int>("n_process_num")();
+    *process_num++;
 
-    boost::interprocess::managed_shared_memory shared_memory(
-        boost::interprocess::open_or_create, shared_memory_name.c_str(), 10000 * sizeof(char));
+    chat* ptr = shared_memory.find_or_construct<chat>("n_Chat")();
 
-    boost::interprocess::mapped_region region(shared_memory, boost::interprocess::read_write);
+    //started separate the thread in process fo reading and writing
+    std::thread read_thread(thread_func_read);
+    std::thread write_thread(thread_func_write);
 
-    std::cout << "shared memory ready" << std::endl;
-
-    system("pause");
-
-   // boost::interprocess::shared_memory_object::remove(shared_memory_name.c_str()); // !
-
-   //make two threads with one mutex 
-   //wait two threads to join (join when completed)
-
+    *process_num--;
+    if (*process_num == 0 ) shared_memory_object::remove(shared_memory_name.c_str()); // !
+    
 
     return EXIT_SUCCESS;
 }
